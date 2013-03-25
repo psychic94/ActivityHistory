337d65f6c2c9953e0d4670366fe913a4a15beaf3
psychic94 <psychic94@gmail.com>
2013-03-25 17:55:29 -0400

    Added a default value system

--- psy/ActivityHistory/PlayerQueryCommandExecutor.java
+++ psy/ActivityHistory/PlayerQueryCommandExecutor.java
@@ -17,17 +17,22 @@ public abstract class PlayerQueryCommandExecutor implements CommandExecutor{
     
     @SuppressWarnings("deprecation")
     protected Date timeStringToDate(String str){
+        Date now = new Date();
+        Integer day = 1, month = now.getMonth(), year = now.getYear();
+        Integer hour = now.getHours(), minute = 0, second = 0;
         String[] str2 = str.split("-");
-        String[] date = str2[0].split("/");
-        String[] time = str2[1].split(":");
-        Integer[] ints = {
-            new Integer(date[2]) + 100,
-            new Integer(date[0]) - 1,
-            new Integer(date[1]),
-            new Integer(time[0]),
-            new Integer(time[1]),
-            new Integer(time[2]),
-        };
-        return new Date(ints[0], ints[1], ints[2], ints[3], ints[4], ints[5]);
+        if(!str2[0].trim().equals("")){
+            String[] date = str2[0].split("/");
+            if(date.length >= 1) month = new Integer(date[0]) - 1;
+            if(date.length >= 2) day = new Integer(date[1]);
+            if(date.length >= 3) year = new Integer(date[2]) + 100;
+        }
+        if(!str2[1].trim().equals("")){
+            String[] time = str2[1].split(":");
+            if(time.length >= 1) hour = new Integer(time[0]);
+            if(time.length >= 2) minute = new Integer(time[1]);
+            if(time.length >= 3) second = new Integer(time[2]);
+        }
+        return new Date(year, month, day, hour, minute, second);
     }
 }
