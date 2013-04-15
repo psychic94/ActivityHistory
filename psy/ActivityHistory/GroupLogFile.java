26f49dcace6ce2e11a5017a4808a08880712ebd1
psychic94 <psychic94@gmail.com>
2013-04-15 19:15:53 -0400

    Beginning to implement group queries

---
+++ psy/ActivityHistory/GroupLogFile.java
@@ -0,0 +1,195 @@
+package psy.ActivityHistory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Stack;
+import java.util.EmptyStackException;
+
+import psy.util.TimeRange;
+
+public class GroupLogFile{
+    private HashMap<Date, Integer> sessions;
+    private HashMap<Date, HashMap<String, Integer>> demogrphx;
+    private Date firstSession;
+    private File file;
+    
+    @SuppressWarnings("unchecked")
+    public GroupLogFile(String pathname){
+        file = new File(pathname);
+        try{
+            file.createNewFile();
+        }catch(Exception e){
+        }
+        sessions = new HashMap();
+        firstSession = null;
+        loadSessions();
+        firstSession = getFirstSession();
+    }
+    
+    //Returns true if loading successful, false if an error was caught
+    private boolean loadSessions(){
+        BufferedReader br = reader();
+        while(true){
+            String line = new String();
+            try{
+                line = br.readLine();
+            }catch(IOException e){
+                break;
+            }catch(NullPointerException e){
+                break;
+            }
+            if(line==null) break;
+            else if (line.trim().equals("")) continue;
+            String[] data = line.split(":");
+            Date date;
+            Integer len;
+            try{
+                date = new Date(new Long(data[0]));
+                len = new Integer(data[1]);
+            }catch(NumberFormatException e){
+                continue;
+            }
+            data = data[2].split(",");
+            HashMap temp = new HashMap<String, Integer>();
+            for(String datum : data){
+                String[] temp2 = datum.trim().split(" ");
+                temp.put(temp2[1], new Integer(temp2[0]));
+            }
+            sessions.put(date, len);
+            demogrphx.put(date, temp);
+        }
+        //Save any changes made when fixing invalid data
+        return saveSessions();
+    }
+    
+    //Returns true if saving was successful, false if an error was caught
+    @SuppressWarnings("unchecked")
+    private boolean saveSessions(){
+        BufferedWriter bw = writer(false);
+        for(Date key : sessions.keySet()){
+            String line = key.getTime() + ":" + sessions.get(key) + ": ";
+            HashMap<String, Integer> temp = demogrphx.get(key);
+            for(String key2 : temp.keySet()){
+                line += temp.get(key2) + " " + key2 + ", ";
+            }
+            try{
+                bw.write(line);
+                bw.newLine();
+            }catch(IOException e){
+                continue;
+            }
+        }
+        try{
+            bw.flush();
+        }catch(IOException e){
+            return false;
+        }catch(NullPointerException e){
+            return false;
+        }
+        try{
+            bw.close();
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    //Returns true if addition was successful, false if an error was caught
+    public boolean addSession(long time, int len, HashMap<String, Integer> map){
+        BufferedWriter bw = writer(true);
+        String line = "" + time + ":" + len + ": ";
+        for(String key : map.keySet()){
+            line += map.get(key) + " " + key + ", ";
+        }
+        try{
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }catch(IOException e){
+            return false;
+        }
+        return true;
+    }
+    
+    @SuppressWarnings("deprecation")
+    public String tallyActivityTotal(TimeRange range){
+        if(range.getStart() == null) range.setStart(firstSession);
+        int time = 0;
+        for(Date date : sessions.keySet()){
+            if(range.includes(date) || range.getStart().equals(firstSession))
+                time+=sessions.get(date);
+        }
+        if(time == -1 || range.getStart() == null) return "There is no record of that player.";
+        int hours = time / 60, minutes = time % 60;
+        return "" + hours + "hours" + minutes + "minutes";
+    }
+    
+    @SuppressWarnings("deprecation")
+    public double tallyActivityPercent(TimeRange range, int hour){
+        if(range.getStart() == null) range.setStart(firstSession);
+        int time = 0;
+        for(Date date : sessions.keySet()){
+            if((range.includes(date) || range.getStart().equals(firstSession)) && (hour == -1 || date.getHours() == hour))
+                time+=sessions.get(date);
+        }
+        if(time == -1 || range.getStart() == null) return -1;
+        long startLong = new Long(range.getStart().getTime());
+        long dateLong = new Long((new Date()).getTime());
+        long timeDiff = dateLong - startLong;
+        timeDiff /= 1000;
+        timeDiff /= 60;
+        if(hour != -1)
+            timeDiff /= 24;
+        time *= 100;
+        double percent = ((double)time)/timeDiff;
+        percent *= 100;
+        percent = Math.round(percent);
+        percent /= 100;
+        return percent;
+    }
+    
+    private BufferedReader reader(){
+        try{
+            return new BufferedReader(new FileReader(file));
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private BufferedWriter writer(boolean append){
+        try{
+            return new BufferedWriter(new FileWriter(file, append));
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    @SuppressWarnings("deprecation")
+    private boolean matchesConditions(Date date, Date start, Date end, int hour){
+        if(!date.before(end))
+            return false;
+        if(start != null && !date.after(start) && !date.equals(start))
+            return false;
+        if(hour != -1 && date.getHours() != hour)
+            return false;
+        return true;
+    }
+    
+    private Date getFirstSession(){
+        Date first = new Date();
+        for(Date date : sessions.keySet())
+            if(date.before(first))
+                first = date;
+        return first;
+    }
+}
--- psy/ActivityHistory/cmd/FileGQCE.java
+++ psy/ActivityHistory/cmd/FileGQCE.java
@@ -1,169 +1,151 @@
 package psy.ActivityHistory.cmd;
 
 import java.util.Date;
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import psy.ActivityHistory.ActivityHistory;
+import psy.ActivityHistory.GroupLogFile;
+import psy.util.TimeRange;
 
 import psy.util.TimeRange;
 
 public class FileGQCE extends GroupQueryCommandExecutor{
     private ActivityHistory plugin;
-    private static final Logger logger = Logger.getLogger("Minecraft");
     public FileGQCE(Plugin pl){
         plugin = (ActivityHistory) pl;
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        if(args.length < 1)
+        if(args.length < 1){
             return false;
-        else if(args[0].equalsIgnoreCase("staffdist"))
-            return staffDist(sender, cmd, label, args);
-        else
-            return false;
-    }
-
-    @SuppressWarnings({"deprecation", "unchecked"})
-    private boolean staffDist(CommandSender sender, Command cmd, String label, String[] args){
-        BufferedReader filebr = null;
-        int hour = -1;
-        Date start = null;
-        Date end = new Date();
+        }
+        
+        GroupLogFile file = null;
         try{
-            filebr = loadLogFile("groups");
+            file = loadLogFile();
         }catch(FileNotFoundException e){
             sender.sendMessage("Could not find the log file.");
             return true;
+        }catch(IOException e){
+            sender.sendMessage("An error occurred when loading the log file.");
+            return true;
         }
-
-        // args: staffdist <start>
-        if(args.length == 2){
-            try{
-                start = timeStringToDate(args[1]);
-            }catch(Exception e){
-                sender.sendMessage("Error while parsing the start date. Use format MM/DD/YY-hh:mm:ss");
-                return true;
-            }
-        }
-        // args: staffdist <start> <end>
-        else if(args.length == 3){
-            try{
-                start = timeStringToDate(args[1]);
-            }catch(Exception e){
-                sender.sendMessage("Error while parsing the start date. Use format MM/DD/YY-hh:mm:ss");
-                return true;
-            }
-            try{
-                end = timeStringToDate(args[2]);
-            }catch(Exception e){
-                sender.sendMessage("Error while parsing the end date. Use format MM/DD/YY-hh:mm:ss");
-                return true;
-            }
-        }else{
-            return false;
-        }
-
-        TimeRange range = new TimeRange(start, end);
-        double[] pcnt = new double[24];
-        int[] denom = new int[24];
+            
         Player player = null;
         if(sender instanceof Player)
             player = (Player) sender;
-
-        try{
-            String line = filebr.readLine();
-            while(line != null){
-                String[] info = line.split(":");
-                Date date = new Date(new Long(info[0]));
-                if(range.includes(date)){
-                    String[] groups = info[1].split(",");
-                    int total = 0;
-                    int staff = 0;
-                    for(String group : groups){
-                        if(!group.equals(" ") && !group.equals("")){
-                            group = group.trim();
-                            int space = group.indexOf(" ");
-                            try{
-                                int number = new Integer(group.substring(0, space));
-                                group = group.substring(space+1);
-                                total += number;
-                                if(Arrays.asList(plugin.config.getStringList("groups.staffGroups")).contains(group))
-                                    staff += number;
-                            }catch(Exception e){
-                            }
-                        }
-                    }
-                    denom[date.getHours()]++;
-                    pcnt[date.getHours()] += (((double)staff)/total);
-                }
-                line = filebr.readLine();
+            
+        String mode = cmd.getName();
+        TimeRange range = CmdUtils.parseRange(sender, args);
+        if(range==null) return true;
+        if(mode.equalsIgnoreCase("ppercent")){
+            Integer hour = CmdUtils.parseHour(sender, args);
+            if(hour==null) return true;
+            double percent = file.tallyActivityPercent(range, hour);
+            if(percent<=0) sender.sendMessage("There is no record of that player.");
+            else sender.sendMessage("" + percent + "%");
+        }else if(mode.equalsIgnoreCase("ptotal")){
+            sender.sendMessage(file.tallyActivityTotal(range));
+        }else if(mode.equalsIgnoreCase("phours")){
+            double[] data = new double[24];
+            String[] messages = {"", "", "", ""};
+            for(int i=0; i<24; i++){
+                data[i] = file.tallyActivityPercent(range, i);
+                messages[i/6] += "" + i + ":00- " + data[i] + "%   ";
             }
-         }catch(IOException e){
-            if(player != null)
-                player.sendMessage("An error occured while processing the logs.");
-            else
-                logger.log(Level.WARNING, "An error occured while processing the logs.");
-            return true;
+            for(String message : messages)
+                sender.sendMessage(message);
         }
-        for(int i=0; i<24; i++){
-            pcnt[i] = pcnt[i]/denom[i];
-        }
-        String[] graph = new String[20];
-        for(int i=19; i>=0; i--){
-            String line = "";
-            for(int j=0; j<24; j++){
-                if(pcnt[j]>=(i*5))
-                    line += "#";
-                else
-                    line += "-";
-            }
-            graph[i] = line;
-        }
-        for(String line : graph)
-            sender.sendMessage(line);
-        hour = 0;
-        double num = pcnt[0];
-        for(int i=1; i<24; i++){
-            if(num < pcnt[i]){
-                hour = i;
-                num = pcnt[i];
-            }
-        }
-        sender.sendMessage("Staff peak is " + num + "% at " + hour + ".");
         return true;
-        }
-
-    private BufferedReader loadLogFile(String name)throws FileNotFoundException{
-        String filename = plugin.accessConfig().getString("general.logFilesLocation") + "/" + name.toLowerCase() + ".log";
-        File file = new File(filename);
-        FileReader filer = new FileReader(file);
-        return new BufferedReader(filer);
     }
 
-    @SuppressWarnings("deprecation")
-    private Date timeStringToDate(String str) throws Exception{
-        String[] str2 = str.split("-");
-        String[] date = str2[0].split("/");
-        String[] time = str2[1].split(":");
-        Integer[] ints = {
-            new Integer(date[2]) + 100,
-            new Integer(date[0]) - 1,
-            new Integer(date[1]),
-            new Integer(time[0]),
-            new Integer(time[1]),
-            new Integer(time[2]),
-        };
-        return new Date(ints[0], ints[1], ints[2], ints[3], ints[4], ints[5]);
+//     @SuppressWarnings({"deprecation", "unchecked"})
+//     private boolean staffDist(CommandSender sender, Command cmd, String label, String[] args){
+//         GroupLogFile file = loadLogFile();
+//         int hour = -1;
+//         Date start = null;
+//         Date end = new Date();
+//         
+//         //Parse args
+// 
+//         TimeRange range = new TimeRange(start, end);
+//         double[] pcnt = new double[24];
+//         int[] denom = new int[24];
+//         Player player = null;
+//         if(sender instanceof Player)
+//             player = (Player) sender;
+// 
+//         try{
+//             String line = filebr.readLine();
+//             while(line != null){
+//                 String[] info = line.split(":");
+//                 Date date = new Date(new Long(info[0]));
+//                 if(range.includes(date)){
+//                     String[] groups = info[1].split(",");
+//                     int total = 0;
+//                     int staff = 0;
+//                     for(String group : groups){
+//                         if(!group.equals(" ") && !group.equals("")){
+//                             group = group.trim();
+//                             int space = group.indexOf(" ");
+//                             try{
+//                                 int number = new Integer(group.substring(0, space));
+//                                 group = group.substring(space+1);
+//                                 total += number;
+//                                 if(Arrays.asList(plugin.config.getStringList("groups.staffGroups")).contains(group))
+//                                     staff += number;
+//                             }catch(Exception e){
+//                             }
+//                         }
+//                     }
+//                     denom[date.getHours()]++;
+//                     pcnt[date.getHours()] += (((double)staff)/total);
+//                 }
+//                 line = filebr.readLine();
+//             }
+//          }catch(IOException e){
+//             if(player != null)
+//                 player.sendMessage("An error occured while processing the logs.");
+//             else
+//                 logger.log(Level.WARNING, "An error occured while processing the logs.");
+//             return true;
+//         }
+//         for(int i=0; i<24; i++){
+//             pcnt[i] = pcnt[i]/denom[i];
+//         }
+//         String[] graph = new String[20];
+//         for(int i=19; i>=0; i--){
+//             String line = "";
+//             for(int j=0; j<24; j++){
+//                 if(pcnt[j]>=(i*5))
+//                     line += "#";
+//                 else
+//                     line += "-";
+//             }
+//             graph[i] = line;
+//         }
+//         for(String line : graph)
+//             sender.sendMessage(line);
+//         hour = 0;
+//         double num = pcnt[0];
+//         for(int i=1; i<24; i++){
+//             if(num < pcnt[i]){
+//                 hour = i;
+//                 num = pcnt[i];
+//             }
+//         }
+//         sender.sendMessage("Staff peak is " + num + "% at " + hour + ".");
+//         return true;
+//     }
+
+    private GroupLogFile loadLogFile()throws IOException{
+        String filename = plugin.accessConfig().getString("general.logFilesLocation") + "/groups.log";
+        GroupLogFile file = new GroupLogFile(filename);
+        return file;
     }
 }
