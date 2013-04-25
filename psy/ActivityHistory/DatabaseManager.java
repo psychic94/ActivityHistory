cf8b48828ac1ef1080d53529ed506b95a572a431
psychic94 <psychic94@gmail.com>
2013-04-25 15:14:23 -0300

    Implemented logging of players

--- psy/ActivityHistory/DatabaseManager.java
+++ psy/ActivityHistory/DatabaseManager.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 public class DatabaseManager{
     private ActivityHistory plugin;
     private static final Logger logger = Logger.getLogger("Minecraft");
+    public static surveyer;
     Connection con;
     public DatabaseManager(ActivityHistory ah) throws SQLException{
         plugin = ah;
@@ -18,6 +19,33 @@ public class DatabaseManager{
             "jdbc:mysql://" + plugin.accessConfig().getString("SQL.ip") + ":" +
             plugin.accessConfig().getString("SQL.port") + "/", conProps
         );
-        logger.log(Level.INFO, "Successfully connected to database");
+        logger.log(Level.INFO, ActivityHistory.messages.getString("info.dbConnect");
+        surveyer = new SQLServeyer();
+        Statement stmt = con.createStatement();
+        String sql = "CREATE TABLE Players(EntryID int NOT NULL AUTO_INCREMENT, SurveyTime timestamp NOT NULL, SurveyInterval int NOT NULL, PlayerName varchar(20) NOT NULL, PRIMARY KEY (EntryID))";
+        stmt.executeUpdate(sql);
+        sql = "CREATE TABLE Groups(EntryID int NOT NULL AUTO_INCREMENT, SurveyTime timestamp NOT NULL, SurveyInterval int NOT NULL, GroupName text NOT NULL, GroupCount int NOT NULL, PRIMARY KEY (EntryID))";
+        stmt.executeUpdate(sql);
+    }
+    
+    private class SQLServeyer implements Runnable{
+        @SuppressWarnings("unchecked")
+        public void run(){
+            HashMap<String, Integer> demogrphx = new HashMap();
+            long time = (new Date()).getTime();
+            Player[] players = plugin.getServer().getOnlinePlayers();
+            if(plugin.accessConfig().getBoolean("players.enabled")){
+                for(Player player : players){
+                    try{
+                        Statement stmt = con.createStatement();
+                        String sql = "INSERT INTO Players (SurveyTime, SurveyInterval, PlayerName) VALUES (";
+                        sql +=  new Timestamp(time) + ", " + plugin.accessConfig().getInt("general.surveyInterval") + ", " + player.getName() + ")";
+                        stmt.executeUpdate(sql);
+                    }catch(Exception e){
+                        plugin.logException(e, player.getName());
+                    }
+                }
+            }
+        }
     }
 }
