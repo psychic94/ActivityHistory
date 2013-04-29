20b8741f5c8adab6df61a98b7cf65360b9693baf
psychic94 <psychic94@gmail.com>
2013-04-29 01:01:18 -0400

    Added support for localization

--- psy/ActivityHistory/cmd/DisabledGQCE.java
+++ psy/ActivityHistory/cmd/DisabledGQCE.java
@@ -4,11 +4,14 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import psy.ActivityHistory.ActivityHistory;
+
 public class DisabledGQCE extends GroupQueryCommandExecutor{
     public DisabledGQCE(Plugin pl){
     }
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        sender.sendMessage(ActivityHistory.messages.getString("info.playerDisabled"));
         return true;
     }
 }
--- psy/ActivityHistory/cmd/DisabledPQCE.java
+++ psy/ActivityHistory/cmd/DisabledPQCE.java
@@ -4,12 +4,14 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import psy.ActivityHistory.ActivityHistory;
+
 public class DisabledPQCE extends PlayerQueryCommandExecutor{
     public DisabledPQCE(Plugin pl){
     }
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        sender.sendMessage("Player queries are disabled at this time.");
+        sender.sendMessage(ActivityHistory.messages.getString("info.playerDisabled"));
         return true;
     }
 }
