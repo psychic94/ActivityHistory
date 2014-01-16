eecd49a6f87a29c2db8d719dcf249e67a27140a2
psychic94 <psychic94@gmail.com>
2014-01-15 21:27:58 -0500

    Further implemented SQL mode

--- psy/ActivityHistory/DatabaseManager.java
+++ psy/ActivityHistory/DatabaseManager.java
@@ -39,26 +39,18 @@ public class DatabaseManager{
         
         DatabaseMetaData meta = con.getMetaData();
         ResultSet tables = meta.getTables(null, null, "Players", null);
-        if(!tables.next()) createPlayerTable();
+        if(!tables.first()) createPlayerTable();
         
         tables = meta.getTables(null, null, "Groups", null);
-        if(!tables.next()) createGroupTable();
+        if(!tables.first()) createGroupTable();
     }
     
     public void createPlayerTable() throws SQLException{
         Statement stmnt = con.createStatement();
         stmnt.executeUpdate(
-            "CREATE TABLE Attendance("
-            + "EntryID INT NOT NULL AUTO_INCREMENT, Player VARCHAR(16) NOT NULL, "
-            + "Day DATE NOT NULL, Hour0 BINARY(60), Hour1 BINARY(60), "
-            + "Hour2 BINARY(60), Hour3 BINARY(60), Hour4 BINARY(60), "
-            + "Hour5 BINARY(60), Hour6 BINARY(60), Hour7 BINARY(60), "
-            + "Hour8 BINARY(60), Hour9 BINARY(60), Hour10 BINARY(60), "
-            + "Hour11 BINARY(60), Hour12 BINARY(60), Hour13 BINARY(60), "
-            + "Hour14 BINARY(60), Hour15 BINARY(60), Hour16 BINARY(60), "
-            + "Hour17 BINARY(60), Hour18 BINARY(60), Hour19 BINARY(60), "
-            + "Hour20 BINARY(60), Hour21 BINARY(60), Hour22 BINARY(60), "
-            + "Hour23 BINARY(60), PRIMARY KEY (EntryID))"
+            "CREATE TABLE Players("
+            + "EntryID INT NOT NULL AUTO_INCREMENT, PlayerName VARCHAR(16) NOT NULL, "
+            + "Start DATE NOT NULL, End DATE NOT NULL, PRIMARY KEY (EntryID))"
         );
     }
     
@@ -72,27 +64,27 @@ public class DatabaseManager{
     
     public double tallyActivityPercent(TimeRange range, int hour, String playername){
         try{
+        	//Get data
             Statement stmt = con.createStatement();
             String sql = "SELECT * FROM Players WHERE PlayerName = " + playername;
-            sql += "ORDER BY SurveyTime";
+            sql += "AS Sessions ORDER BY Start";
             ResultSet result = stmt.executeQuery(sql);
+            //Set the range start, if necessary
             if(range.getStart() == null){
-                range.setStart(result.getTimestamp(1));
+            	result.first();
+                range.setStart(result.getDate(3));
             }
-            Timestamp start = new Timestamp(range.getStart().getTime());
-            Timestamp end = new Timestamp(range.getEnd().getTime());
-            sql = "SELECT SUM(SurveyInterval) FROM " + result + " WHERE SurveyTime BETWEEN " + start + " AND " + end;
-            int time = stmt.executeQuery(sql).getInt(1);
             
-            long startLong = new Long(range.getStart().getTime());
-            long endLong = new Long(range.getEnd().getTime());
-            long timeDiff = endLong - startLong;
-            timeDiff /= 1000;
-            timeDiff /= 60;
-            if(hour != -1)
-               timeDiff /= 24;
-            time *= 100;
-            double percent = ((double)time)/timeDiff;
+            //Sum the overlaps of the sessions and the search TimeRange
+            long ontime = 0;
+            result.beforeFirst();
+            while(result.next()){
+            	TimeRange temp = new TimeRange(result.getDate(3), result.getDate(4));
+            	ontime += range.overlap(temp);
+            }
+            
+            //Calculate activity percent to two decimal places
+            double percent = ontime/range.length();
             percent *= 100;
             percent = Math.round(percent);
             percent /= 100;
--- psy/ActivityHistory/cmd/DatabasePQCE.java
+++ psy/ActivityHistory/cmd/DatabasePQCE.java
@@ -1,17 +1,15 @@
 package psy.ActivityHistory.cmd;
 
-import org.bukkit.plugin.Plugin;
-import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
 import psy.ActivityHistory.ActivityHistory;
-import psy.ActivityHistory.DatabaseManager;
 import psy.util.TimeRange;
 
 public class DatabasePQCE extends PlayerQueryCommandExecutor{
-    private ActivityHistory plugin;
     public DatabasePQCE(Plugin pl){
-        plugin = (ActivityHistory)pl;
     }
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
@@ -22,19 +20,26 @@ public class DatabasePQCE extends PlayerQueryCommandExecutor{
         Player player = null;
         if(sender instanceof Player)
             player = (Player) sender;
-            
+        
+        //Analyze command
         String mode = cmd.getName();
         TimeRange range = CmdUtils.parseRange(sender, args, 1);
         if(range==null) return true;
+        
+        //Player activity percent
         if(mode.equalsIgnoreCase("ppercent")){
             Integer hour = CmdUtils.parseHour(sender, args, 1);
             if(hour==null) return true;
             double percent = ActivityHistory.dbm.tallyActivityPercent(range, hour, args[0]);
             if(percent<=0) sender.sendMessage(ActivityHistory.messages.getString("errors.playerNotFound"));
             else sender.sendMessage("" + percent + "%");
-        }else if(mode.equalsIgnoreCase("ptotal")){
+        }
+        //Player total ontime
+        else if(mode.equalsIgnoreCase("ptotal")){
             //sender.sendMessage(file.tallyActivityTotal(range));
-        }else if(mode.equalsIgnoreCase("phours")){
+        }
+        //Player activity percent by hour
+        else if(mode.equalsIgnoreCase("phours")){
             double[] data = new double[24];
             String[] messages = {"", "", "", ""};
             for(int i=0; i<24; i++){
